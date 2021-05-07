83323c896192206a5ef9e4886efa9a11aacdbd4c
Vidyayani <vidyayani@gmail.com>
2021-05-08 00:18:44 +0530

    Updated spend api logic

--- routes/spend.js
+++ routes/spend.js
@@ -1,7 +1,21 @@
 const router = require('express').Router()
+const transaction = require('../models/Transaction')
+const user = require('../models/User')
 
-router.get('/', (req,res) => {
-    res.status(200).json('/spend')
+router.post('/', (req,res) => {
+    let spend = parseFloat(req.body.amount)
+    let tkn = req.headers.authorization
+    
+    user.hasFunds(tkn,spend).then((bool) => {
+    if(bool){
+    let trans = new transaction(tkn,req.body.date,req.body.description,req.body.amount, req.body.currency)
+    user.updateUser(tkn,spend)
+    res.status(200).json(trans)
+    }else{
+    res.status(400).json({ "error": "Insufficient funds"})
+    }
+})
+
 })
 
 module.exports = router
